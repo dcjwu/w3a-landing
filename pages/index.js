5bab59bfe826b4dcb4277a3732844c54609e8b45
AK <kkucenko.aleksejs@gmail.com>
2022-02-13 19:53:19 +0200

    Added SEO tags.

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,14 @@ export default function Home() {
    return (
       <div className={styles.container}>
          <Head>
-            <title>Web3App Agency</title>
-            <meta content="Web 3 application design, development, testing, deployment and support" name="description" />
+            <title>Web3App Agency | Your Blockchain Development Solution</title>
+            <meta content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport" />
+            <meta charSet="utf-8" />
+            <meta content="en" httpEquiv="content-language"/>
+            <meta content="Web3App Agency is a custom software development company that delivers top-level solutions" name="description"/>
+            <meta content="Web3App Agency | Your Blockchain Development Solution" property="og:title"/>
+            <meta content="website" property="og:type"/>
+            <meta content="Web3App Agency is a custom software development company that delivers top-level solutions" property="og:description"/>
             <link href="/favicon.ico" rel="icon" />
          </Head>
 
