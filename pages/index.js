b1f5d5dc6c2f9f3062e3b029697d17f5d946551e
AK <kkucenko.aleksejs@gmail.com>
2022-02-14 10:58:19 +0200

    Minor title change.

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
    return (
       <>
          <Head>
-            <title>Web3App Agency | Your Blockchain Development Solution</title>
+            <title>Web3App Agency | High Quality Blockchain Development</title>
             <meta content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=0"
                   name="viewport"/>
             <meta charSet="utf-8"/>
             <meta content="en" httpEquiv="content-language"/>
             <meta content="Web3App Agency is a custom software development company that delivers top-level solutions"
                   name="description"/>
-            <meta content="Your Blockchain Development Solution" property="og:title"/>
+            <meta content="High Quality Blockchain Development" property="og:title"/>
             <meta content="website" property="og:type"/>
             <meta content="Web3App Agency is a custom software development company that delivers top-level solutions"
                   property="og:description"/>
