1dacab0ac5921b7f322b6bef6fc70a2fd8fc9561
AK <kkucenko.aleksejs@gmail.com>
2022-02-17 23:05:07 +0200

    It comes that preloader is not required, as builded website laods faster than locally with preloaded styles.

--- pages/_app.js
+++ pages/_app.js
@@ -11,7 +11,7 @@ function MyApp({ Component, pageProps }) {
             <link href="https://fonts.googleapis.com" rel="preconnect"/>
             <link crossOrigin="anonymous" href="https://fonts.gstatic.com" rel="preconnect"/>
             <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;700&family=Roboto:wght@300;400;500;700&family=Source+Code+Pro:wght@300;400;500;700&display=swap"
-                     rel="stylesheet"/>
+               rel="stylesheet"/>
          </Head>
          <Component {...pageProps}/>
       </>
