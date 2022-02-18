197074f5e22041c838b47caa72151b6b9ab16a81
AK <kkucenko.aleksejs@gmail.com>
2022-02-18 23:35:56 +0200

    Fixing Twitter and Facebook.

--- pages/index.js
+++ pages/index.js
@@ -26,8 +26,13 @@ export default function Home() {
             <meta content="Web3App Agency is a custom software development company that delivers top-level solutions"
                   property="og:description"/>
             <meta content="https://web3app.agency/icon/icon-big.png" property="og:image:secure_url"/>
+            <meta content="https://www.web3app.agency/icon/icon-full.png" property="og:image"/>
             <meta content="image/png" property="og:image:type"/>
-            <meta content="https://www.web3app.agency/icon/icon-180x180.png" name="twitter:image"/>
+            <meta content="https://www.web3app.agency/icon/icon-full.png" name="twitter:card"/>
+            <meta content="Web3App Agency | High Quality Blockchain Development" name="twitter:title"/>
+            <meta content="Web3App Agency is a custom software development company that delivers top-level solutions"
+                  name="twitter:description"/>
+            <meta content="https://web3app.agency/icon/icon-big.png" name="twitter:image"/>
          </Head>
 
 
