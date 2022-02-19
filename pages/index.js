0f1661069a4ff41b304999480d7c8f2b8d657080
AK <kkucenko.aleksejs@gmail.com>
2022-02-19 02:31:41 +0200

    404 error page.

--- pages/index.js
+++ pages/index.js
@@ -42,7 +42,6 @@ export default function Home() {
             <AboutBlock/>
             <TeamBlock/>
             <ContactUsBlock/>
-            {/*<UnderConstruction/>*/}
          </div>
          <Footer/>
       </>
